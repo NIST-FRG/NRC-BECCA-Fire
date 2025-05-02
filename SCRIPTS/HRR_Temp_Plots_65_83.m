b3319d94132fbf8f7aea26f6cb5b7a1391133159
mcgratta <mcgratta@gmail.com>
2025-05-02 17:42:40 -0400

    Add penetration update

--- SCRIPTS/HRR_Temp_Plots_65_83.m
+++ SCRIPTS/HRR_Temp_Plots_65_83.m
@@ -23,7 +23,25 @@ Paper_Height    = 4.5;
 Figure_Visibility = 'on';
 Font_Interpreter = 'LaTeX';
 
-t_ignition  = {1097  676  692 1219  737  1308  2716  2159  2007  2150   622  571 };
+t_fail  = {[  -1   -1 14.2 13.8];... % Test 65
+           [  -1   -1 22.4 23.0];... % Test 66
+           [33.1 27.2 29.9 33.8];... % Test 67
+           [  -1   -1   -1   -1];... % Test 68
+           [  -1   -1   -1   -1];... % Test 69
+           [  -1   -1 30.9 31.0];... % Test 70
+           [16.1 17.5 12.7 13.2];... % Test 71
+           [24.5 20.7 17.7 18.5];... % Test 72
+           [  -1   -1   -1   -1];... % Test 73
+           [  -1   -1 23.7 23.4];... % Test 74
+           [30.0 26.8 21.4 21.2];... % Test 75
+           [  -1   -1   -1   -1];... % Test 76
+           [  -1   -1 17.3 16.7];... % Test 77
+           [10.9 10.4  9.6  9.9];... % Test 78
+           [29.0 30.1 30.4 30.8];... % Test 79
+           [  -1   -1   -1   -1];... % Test 80
+           [  -1   -1 30.3 30.5];... % Test 81
+           [17.3 17.3 17.5 17.2];... % Test 82
+           [19.5 17.2 13.2 12.2]};   % Test 83
 
 for j=1:19 % Test
 
@@ -57,6 +75,9 @@ for j=1:19 % Test
    plot(E.data(:,1)/60,E.data(:,5),'k-'); hold on
    plot(E.data(:,1)/60,E.data(:,6),'g-'); hold on
 
+   if t_fail{j}(1)>0 ; plot([t_fail{j}(1) t_fail{j}(1)],[0 800],'k--'); hold on ; end
+   if t_fail{j}(2)>0 ; plot([t_fail{j}(2) t_fail{j}(2)],[0 800],'k--'); hold on ; end
+
    set(gca,'FontName',Font_Name)
    set(gca,'FontSize',Title_Font_Size)
    axis([0 40 0 800])
@@ -80,6 +101,9 @@ for j=1:19 % Test
    plot(E.data(:,1)/60,E.data(:,7),'r-'); hold on
    plot(E.data(:,1)/60,E.data(:,8),'k-'); hold on
    plot(E.data(:,1)/60,E.data(:,9),'g-'); hold on
+
+   if t_fail{j}(3)>0 ; plot([t_fail{j}(3) t_fail{j}(3)],[0 800],'k--'); hold on ; end
+   if t_fail{j}(4)>0 ; plot([t_fail{j}(4) t_fail{j}(4)],[0 800],'k--'); hold on ; end
 
    set(gca,'FontName',Font_Name)
    set(gca,'FontSize',Title_Font_Size)
